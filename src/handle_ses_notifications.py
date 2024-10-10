70c73eaefcc3a1ba889efde604020687c12d4816
claudiomiranda <cronosunder@gmail.com>
2024-10-10 12:05:45 -0300

    fix: Procesar evento de email

--- src/handle_ses_notifications.py
+++ src/handle_ses_notifications.py
@@ -4,6 +4,7 @@ import traceback
 import uuid
 from datetime import datetime, timedelta
 
+from utils import sqs
 from utils.dynamo import get_dynamo_client
 from utils.events import send_event
 
@@ -20,13 +21,7 @@ TTL = int(os.environ.get('TTL', 525600))
 
 
 def handler(message, context):
-    records = message.get('Records', [])
-    for record in records:
-        try:
-            procesar_record(record)
-        except:
-            traceback.print_exc()
-            pass
+    return sqs.procesar_mensajes(message, procesar_record, context)
 
 
 # Funciones utilitarias
@@ -49,12 +44,11 @@ def procesa_record(record):
         message = json.loads(message)
 
 
-def procesar_record(record):
+def procesar_record(record, context):
     sqs_body: dict = record.get('body', '')
     if isinstance(sqs_body, str):
         sqs_body = json.loads(sqs_body)
-
-    message = sqs_body.get('Message', '{}')
+    message = sqs_body.get('Message', sqs_body)
     if isinstance(message, str):
         message = json.loads(message)
     type_ = message['eventType']
@@ -156,3 +150,40 @@ async def procesar_eventos_suppression(type_, event_detail, mail, timestamp, mes
             })
 
 
+if __name__ == "__main__":
+    with open('/Users/claudiomiranda/IdeaProjects/amazon-ses-bounce-dashboard/events/email_received.json', 'r') as f:
+        message = json.load(f)
+        if 'Records' not in message:
+            message = {
+                "Records": [
+                    {
+                        "messageId": "f49f17b1-dd05-41a6-8078-1d2f90f04303",
+                        "receiptHandle": "AQEBeGF66n0QmhIQjN1ViHu4TXkyvnoUYWHqRVU0t+AF7GNNAslJOL9+yqHxNGX0betyYsJgixxVHu7Q1p9CChS7RVEmbLfOBebQx+MGsRwicqRCqLFNKAo/1ZsAlJyL6xPyOpNsPBVxDc3D/La7bTFcYdKv9HNZG0F55c9UJVkmaEW+wc9jV4PW+7IiIMya3k4GUCjWpdHRk3VjHIyQ+qJQfjDbjSoAFgUqjm7SPVmiHH0j9BF16GnusGOq9bJJC6Po3l3c9PUSoBGbZ4MUfoKvj/BOcd/k67tBsNwh3vY34yZjCpk+wq9XcThR+phdJF/LHcu5QBi1aE4EOFMjGgqZD9pv/5z60pfUVNKmiZjH2FGwBe2VfP9ajCsK1Y1Pqrul",
+                        "body": "{\"eventType\":\"Open\",\"mail\":{\"timestamp\":\"2024-10-10T14:32:47.835Z\",\"source\":\"notificaciones@empresas.febos.cl\",\"sendingAccountId\":\"830321976775\",\"messageId\":\"0100019276d8b9db-68b3478b-d341-4361-9888-11e7d39ba92d-000000\",\"destination\":[\"jlmingo@interkambio.cl\"],\"headersTruncated\":false,\"headers\":[{\"name\":\"Content-Type\",\"value\":\"multipart/mixed; boundary=\\\"===============4188404272342821134==\\\"\"},{\"name\":\"MIME-Version\",\"value\":\"1.0\"},{\"name\":\"From\",\"value\":\"Febos <notificaciones@empresas.febos.cl>\"},{\"name\":\"Subject\",\"value\":\"Envío de DTE\"},{\"name\":\"To\",\"value\":\"jlmingo@interkambio.cl\"}],\"commonHeaders\":{\"from\":[\"Febos <notificaciones@empresas.febos.cl>\"],\"to\":[\"jlmingo@interkambio.cl\"],\"messageId\":\"0100019276d8b9db-68b3478b-d341-4361-9888-11e7d39ba92d-000000\",\"subject\":\"Envío de DTE\"},\"tags\":{\"ses:source-tls-version\":[\"TLSv1.3\"],\"ses:operation\":[\"SendRawEmail\"],\"ses:configuration-set\":[\"default\"],\"stage\":[\"produccion\"],\"ses:source-ip\":[\"3.230.171.119\"],\"ses:from-domain\":[\"empresas.febos.cl\"],\"ses:caller-identity\":[\"ses-event-manager-ProcessSESSendQueueRole-T37E7DP89RXL\"],\"empresa\":[\"85218700-K\"]}},\"open\":{\"timestamp\":\"2024-10-10T14:32:55.387Z\",\"userAgent\":\"Mozilla/5.0 (Windows NT 5.1; rv:11.0) Gecko Firefox/11.0 (via ggpht.com GoogleImageProxy)\",\"ipAddress\":\"66.249.84.71\"}}\n",
+                        "attributes": {
+                            "ApproximateReceiveCount": "1",
+                            "SentTimestamp": "1728570775505",
+                            "SenderId": "AIDAIT2UOQQY3AUEKVGXU",
+                            "ApproximateFirstReceiveTimestamp": "1728570775510"
+                        },
+                        "messageAttributes": {},
+                        "md5OfBody": "7610a5fcb32508ce19a1507802089e18",
+                        "eventSource": "aws:sqs",
+                        "eventSourceARN": "arn:aws:sqs:us-east-1:830321976775:ses-event",
+                        "awsRegion": "us-east-1"
+                    }
+                ]
+            }
+
+
+        class Contexto:
+            aws_request_id = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+            log_group_name = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+            log_stream_name = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+            def __int__(self):
+                self.aws_request_id = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+
+        print(json.dumps(message))
+        handler(message, Contexto())
